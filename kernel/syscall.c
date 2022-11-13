4b0a39eb7b676fe795688fa5a513cd03ebf1cc71
anubis-robot <anubis@anubis-lms.io>
2022-11-13 19:14:45 +0000

    Anubis Cloud IDE Autosave netid=srm9547

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -95,6 +95,7 @@ static int (*syscalls[])(void) = {
     [SYS_sleep] sys_sleep, [SYS_uptime] sys_uptime, [SYS_open] sys_open,
     [SYS_write] sys_write, [SYS_mknod] sys_mknod,   [SYS_unlink] sys_unlink,
     [SYS_link] sys_link,   [SYS_mkdir] sys_mkdir,   [SYS_close] sys_close,
+    [SYS_nice] sys_nice
 };
 
 void syscall(void) {
