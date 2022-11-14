2cc1b6440fddff4ca1f0e4c7a8b76bc922be94d0
anubis-robot <anubis@anubis-lms.io>
2022-11-14 13:54:36 +0000

    Anubis Cloud IDE Autosave netid=srm9547

--- user/randomtest.c
+++ user/randomtest.c
@@ -1,17 +1,23 @@
 #include "kernel/random.h"
+#include "user.h"
 
 #define N 1000
 
 int main(void) {
     srand(10);
-    int min = rand(), max = rand(), mean = 0;
-    int t_count = 0;
+    int t = rand();
+    int min = t, max = t;
+    float mean = t;
+    int t_count = 1;
     for(int i=0;i<N-1;i++){
-        int t = rand();
+        t = rand();
+        t_count++;
         if(t < min)
             min = t;
         if(t > max)
             max = t;
-        
+        float newMean = (t-mean) / t_count;
+        mean += newMean;
     }
+    printf(1, "Min: %d, Max: %d, Mean: %f", min, max, mean);
 }
